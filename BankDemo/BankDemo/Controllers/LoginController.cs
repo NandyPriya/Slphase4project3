70d93811428d8332da3708505f2e7d378bf412c7
Nandhinipriya <nandhinipriya745@gmail.com>
2022-10-30 21:18:10 +0530

    Add project files.

---
+++ BankDemo/BankDemo/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(IFormCollection c)
+        {
+            string k = c["username"].ToString();
+            string p = c["password"].ToString();
+            if (k == "nandhu" && p == "N@ndhu")
+            {
+                return RedirectToRoute(new { action = "Index", controller = "Home", area = "" });
+            }
+            else
+            {
+                ViewBag.Message1 = "Invalid username/password";
+                return View();
+            }
+        }
+
+    }
+}
